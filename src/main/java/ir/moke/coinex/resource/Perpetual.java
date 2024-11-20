d6cd0a013090b5c4aa31d3e08bb6ff006af450f7
Mahdi <mah454@mail.moke.ir>
2024-11-20 20:45:49 +0330

    Fix header

--- src/main/java/ir/moke/coinex/resource/Perpetual.java
+++ src/main/java/ir/moke/coinex/resource/Perpetual.java
@@ -4,10 +4,7 @@ import ir.moke.coinex.model.Response;
 import ir.moke.coinex.model.enums.MarketType;
 import ir.moke.coinex.model.enums.OrderSide;
 import ir.moke.coinex.model.request.*;
-import ir.moke.coinex.model.response.MarketResponse;
-import ir.moke.coinex.model.response.OrderResponse;
-import ir.moke.coinex.model.response.StopOrderResponse;
-import ir.moke.coinex.model.response.TickerResponse;
+import ir.moke.coinex.model.response.*;
 import ir.moke.kafir.annotation.GET;
 import ir.moke.kafir.annotation.POST;
 import ir.moke.kafir.annotation.QueryParameter;
@@ -85,4 +82,24 @@ public interface Perpetual {
     @POST("/futures/cancel-stop-order")
     Response<OrderResponse> cancelStopOrder(CancelStopOrder cancelStopOrder);
 
+    @POST("/futures/close-position")
+    Response<OrderResponse> closePosition(ClosePosition closePosition);
+
+    @POST("/futures/adjust-position-margin")
+    Response<PositionResponse> adjustPositionMargin(PositionMargin positionMargin);
+
+    @POST("/futures/adjust-position-leverage")
+    Response<String> adjustPositionLeverage(PositionLeverage leverage);
+
+    @GET("/futures/pending-position")
+    Response<PositionResponse> pendingPosition(@QueryParameter("market") String market,
+                                               @QueryParameter("market_type") MarketType marketType,
+                                               @QueryParameter("page") Integer page,
+                                               @QueryParameter("limit") Integer limit);
+
+    @POST("/futures/set-position-stop-loss")
+    Response<PositionResponse> setPositionStopLoss(PositionStopLoss stopLoss);
+
+    @POST("/futures/set-position-take-profit")
+    Response<PositionResponse> setPositionTakeProfit(PositionTakeProfit takeProfit);
 }
--- src/test/java/ir/moke/sdk/api/PerpetualTest.java
+++ src/test/java/ir/moke/sdk/api/PerpetualTest.java
@@ -1,21 +1,18 @@
 package ir.moke.sdk.api;
 
 import ir.moke.coinex.Coinex;
-import ir.moke.coinex.model.enums.MarketType;
-import ir.moke.coinex.model.enums.OrderSide;
-import ir.moke.coinex.model.enums.OrderType;
-import ir.moke.coinex.model.request.Order;
-import ir.moke.coinex.model.response.TickerResponse;
-import ir.moke.coinex.model.response.MarketResponse;
-import ir.moke.coinex.model.response.OrderResponse;
 import ir.moke.coinex.model.Response;
+import ir.moke.coinex.model.enums.MarginMode;
+import ir.moke.coinex.model.enums.MarketType;
+import ir.moke.coinex.model.request.PositionLeverage;
+import ir.moke.coinex.model.response.MarketResponse;
+import ir.moke.coinex.model.response.TickerResponse;
 import ir.moke.coinex.resource.Perpetual;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class PerpetualTest {
     private static Perpetual perpetual;
 
@@ -32,6 +29,7 @@ public class PerpetualTest {
     }
 
     @Test
+    @org.junit.jupiter.api.Order(0)
     public void market() {
         Response<List<MarketResponse>> response = perpetual.market("BTCUSDT");
         Assertions.assertEquals(0, response.code());
@@ -40,6 +38,7 @@ public class PerpetualTest {
     }
 
     @Test
+    @org.junit.jupiter.api.Order(1)
     public void ticker() {
         Response<List<TickerResponse>> response = perpetual.ticker("BTCUSDT,DOGEUSDT");
         Assertions.assertEquals(0, response.code());
@@ -49,15 +48,25 @@ public class PerpetualTest {
     }
 
     @Test
-    public void order() {
-        Order order = new Order("BTCUSDT", MarketType.FUTURES, OrderSide.SELL, OrderType.MARKET, "1000");
-        Response<OrderResponse> response = perpetual.order(order);
-        System.out.println(response);
-        Assertions.assertNotEquals(0, response.code());
+    public void checkLeverage() {
+        PositionLeverage leverage = new PositionLeverage("BTCUSDT", MarketType.FUTURES, MarginMode.ISOLATED, 3);
+        Response<String> resp = perpetual.adjustPositionLeverage(leverage);
+        System.out.println(resp.code());
+        System.out.println(resp.data());
     }
 
-    @Test
-    public void batchOrderStatus() {
-        Response<List<Response<OrderResponse>>> batchOrderStatus = perpetual.batchOrderStatus("BTCUSDT", "13400");
-    }
+//    @Test
+//    @org.junit.jupiter.api.Order(2)
+//    public void order() {
+//        Order order = new Order("BTCUSDT", MarketType.FUTURES, OrderSide.SELL, OrderType.MARKET, "1000");
+//        Response<OrderResponse> response = perpetual.order(order);
+//        System.out.println(response);
+//        Assertions.assertNotEquals(0, response.code());
+//    }
+//
+//    @Test
+//    @org.junit.jupiter.api.Order(3)
+//    public void batchOrderStatus() {
+//        Response<List<Response<OrderResponse>>> batchOrderStatus = perpetual.batchOrderStatus("BTCUSDT", "13400");
+//    }
 }
